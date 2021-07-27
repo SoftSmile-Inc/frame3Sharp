ef690bcaabf32413286b778080123f6cb40efc28
Artem Makhno <artem.makhno@dataart.com>
2021-07-27 13:40:29 +0300

    #1544 WIP

--- util/DebugUtil.cs
+++ util/DebugUtil.cs
@@ -245,15 +245,11 @@ namespace f3
 
         static public fGameObject EmitDebugMesh(string name, DMesh3 meshIn, Colorf color, GameObject parent = null, bool bIsInWorldPos = true)
         {
-            if (meshIn is null)
-                throw new Exception("Mesh is null");
             DMesh3 mesh = new DMesh3(meshIn);
             if (FPlatform.InMainThread() == false) {
                 ThreadMailbox.PostToMainThread(() => { DebugUtil.EmitDebugMesh(name, mesh, color, parent, bIsInWorldPos); });
                 return null;
             }
-            if (mesh is null)
-                throw new Exception("Mesh is null");
             fMeshGameObject fMeshGO = GameObjectFactory.CreateMeshGO(name, new fMesh(mesh, bAllowLargeMeshes: true), false, true);
             fMeshGO.SetMaterial(MaterialUtil.CreateStandardMaterialF(color));
             if (parent != null)
